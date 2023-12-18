2017663fc1408dca75d6b309fa86fa38f337916a
yuu0252 <yusandao1224@gmail.com>
2023-12-19 01:01:02 +0900

    delete old files

---
+++ .history/src/languageSlice_20231219005337.ts
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const languageSlice = createSlice({
+  name: 'language',
+  initialState: {
+    language: 'en-us',
+  },
+  reducers: {
+    setLanguage: (state, action) => {
+      state.language = action.payload;
+    },
+  },
+});
+
+export const { setLanguage } = languageSlice.actions;
+
+export const selectLanguage = (state: any) => state.language;
+
+export default languageSlice.reducer;
