14ce474cedf0bdb67b488aa8af446c7dd220b2d4
yuu0252 <yutanmaron@gmail.com>
2024-02-22 21:01:13 +0900

    vite setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.scss";
-import { Provider } from "react-redux";
-import store from "./reducer/store.ts";
-import { CookiesProvider } from "react-cookie";
-import App from "./App.tsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import { Provider } from 'react-redux';
+import store from './reducer/store.ts';
+import { CookiesProvider } from 'react-cookie';
+import App from './App.tsx';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CookiesProvider>
       <Provider store={store}>
