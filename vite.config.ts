6f9e5df10cd887cba25d533b866b44429a4e3eac
yuu0252 <yutanmaron@gmail.com>
2024-04-14 19:41:11 +0900

    devide port dev and prod

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 8080,
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      port: mode === "development" ? 3000 : 8080,
+    },
+  };
 });
