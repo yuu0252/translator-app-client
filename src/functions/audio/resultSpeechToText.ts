6f163ebab9311147a6acd2ae8b15aea0f0ec8075
yuu0252 <yutanmaron@gmail.com>
2024-04-23 00:48:38 +0900

    modified doesn't set isJapanese

--- src/functions/audio/resultSpeechToText.ts
+++ src/functions/audio/resultSpeechToText.ts
@@ -1,15 +1,14 @@
-import { languageCodeList } from '../../constants';
-import store from '../../reducer/store';
-import { setCurrentLanguage } from '../../reducer/languageSlice';
-import { setTranscription } from '../../reducer/translateSlice';
-import { TypeResultSpeechToText } from '../../type';
+import { languageCodeList } from "../../constants";
+import store from "../../reducer/store";
+import { setCurrentLanguage, setIsJapanese } from "../../reducer/languageSlice";
+import { setTranscription } from "../../reducer/translateSlice";
+import { TypeResultSpeechToText } from "../../type";
 
 // 入力された音声から翻訳するためのデータを抽出する
 export const resultSpeechToText = (
   result: TypeResultSpeechToText,
   currentLanguage: string | undefined
 ) => {
-  console.log(currentLanguage);
   const detectedLanguage = result.languageCode;
   const text = result.alternatives[0].transcript;
 
@@ -19,24 +18,25 @@ export const resultSpeechToText = (
   let targetLanguage: string | undefined;
 
   // 入力が日本語であるときに出力言語が設定されていなければ英語に変換する
-  if (detectedLanguage === 'ja-jp' && currentLanguage === 'none') {
-    currentLanguage = 'en-us';
-    sourceLanguage = 'ja';
-    targetLanguage = 'en';
-  } else if (detectedLanguage === 'ja-jp') {
+  if (detectedLanguage === "ja-jp" && currentLanguage === "none") {
+    currentLanguage = "en-us";
+    sourceLanguage = "ja";
+    targetLanguage = "en";
+  } else if (detectedLanguage === "ja-jp") {
+    // 出力先言語が指定されていれば翻訳元を日本語に、翻訳先を指定の言語に変換する
     const targetCode = languageCodeList.find((e) => e.code === currentLanguage);
-    sourceLanguage = 'ja';
+    sourceLanguage = "ja";
     targetLanguage = targetCode?.shortCode;
-    console.log(targetLanguage);
+    store.dispatch(setIsJapanese(false));
   } else {
-    if (detectedLanguage !== 'ja-jp' && currentLanguage === 'none') {
+    // 入力言語が日本語以外であればその言語を日本語に翻訳する
+    if (currentLanguage === "none") {
       currentLanguage = detectedLanguage;
     }
-    const sourceCode = languageCodeList.find(
-      (e) => e.code === detectedLanguage
-    );
+    const sourceCode = languageCodeList.find((e) => e.code === currentLanguage);
     sourceLanguage = sourceCode?.shortCode;
-    targetLanguage = 'ja';
+    targetLanguage = "ja";
+    store.dispatch(setIsJapanese(false));
   }
   store.dispatch(setCurrentLanguage(currentLanguage)); // 翻訳する言語をセット
   return [sourceLanguage, targetLanguage, text];
