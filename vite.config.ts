66ddb58a679f8837a38dbfe6cbc9d9f62d61e7e2
yuu0252 <yutanmaron@gmail.com>
2024-04-23 02:24:51 +0900

    add dev command

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    server: {
-      host: true,
-      port: mode === "development" ? 3000 : 8080,
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 8080,
+  },
 });
